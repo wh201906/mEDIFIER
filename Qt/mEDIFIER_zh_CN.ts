bd350e65add56905d2239da9e61eaafa973056fc
wh201906 <wh201906@yandex.com>
2023-08-26 20:31:28 +0800

    Update translations

--- Qt/mEDIFIER_zh_CN.ts
+++ Qt/mEDIFIER_zh_CN.ts
@@ -10,6 +10,12 @@
     </message>
     <message>
         <location filename="devices/basedevice.ui" line="26"/>
+        <location filename="devices/basedevice.cpp" line="188"/>
+        <location filename="devices/basedevice.cpp" line="194"/>
+        <location filename="devices/basedevice.cpp" line="200"/>
+        <location filename="devices/basedevice.cpp" line="206"/>
+        <location filename="devices/basedevice.cpp" line="537"/>
+        <location filename="devices/basedevice.cpp" line="595"/>
         <source>Info</source>
         <translation>信息</translation>
     </message>
@@ -37,214 +43,319 @@
     </message>
     <message>
         <location filename="devices/basedevice.ui" line="110"/>
+        <source>Connect Audio</source>
+        <translation>连接音频</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.ui" line="117"/>
         <source>Name</source>
         <translation>设备名</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="127"/>
+        <location filename="devices/basedevice.ui" line="134"/>
         <source>Takes effect after removing the pairing record and re-pairing</source>
         <translation>删除配对记录并重新配对后生效</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="137"/>
-        <location filename="devices/basedevice.ui" line="217"/>
-        <location filename="devices/basedevice.ui" line="343"/>
-        <location filename="devices/basedevice.ui" line="398"/>
+        <location filename="devices/basedevice.ui" line="144"/>
+        <location filename="devices/basedevice.ui" line="224"/>
+        <location filename="devices/basedevice.ui" line="350"/>
+        <location filename="devices/basedevice.ui" line="407"/>
         <source>Set</source>
         <translation>设置</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="149"/>
-        <location filename="devices/basedevice.ui" line="162"/>
-        <location filename="devices/basedevice.ui" line="242"/>
+        <location filename="devices/basedevice.ui" line="156"/>
+        <location filename="devices/basedevice.ui" line="169"/>
+        <location filename="devices/basedevice.ui" line="249"/>
         <source>Noise Reduction</source>
         <translation>降噪</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="155"/>
-        <location filename="devices/basedevice.ui" line="235"/>
-        <location filename="devices/basedevice.ui" line="277"/>
+        <location filename="devices/basedevice.ui" line="162"/>
+        <location filename="devices/basedevice.ui" line="242"/>
+        <location filename="devices/basedevice.ui" line="284"/>
         <source>Normal</source>
         <translation>标准</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="169"/>
-        <location filename="devices/basedevice.ui" line="179"/>
-        <location filename="devices/basedevice.ui" line="249"/>
+        <location filename="devices/basedevice.ui" line="176"/>
+        <location filename="devices/basedevice.ui" line="186"/>
+        <location filename="devices/basedevice.ui" line="256"/>
         <source>Ambient Sound</source>
         <translation>环境声</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="227"/>
+        <location filename="devices/basedevice.ui" line="234"/>
         <source>Control Settings</source>
         <translation>操控设置</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="258"/>
+        <location filename="devices/basedevice.ui" line="265"/>
         <source>Please select at least 2 items</source>
         <translation>请选择至少2项</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="271"/>
+        <location filename="devices/basedevice.ui" line="278"/>
         <source>Sound Effect</source>
         <translation>音效</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="284"/>
+        <location filename="devices/basedevice.ui" line="291"/>
         <source>Pop</source>
         <translation>流行</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="291"/>
+        <location filename="devices/basedevice.ui" line="298"/>
         <source>Classical</source>
         <translation>古典</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="298"/>
+        <location filename="devices/basedevice.ui" line="305"/>
         <source>Rock</source>
         <translation>摇滚</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="308"/>
+        <location filename="devices/basedevice.ui" line="315"/>
         <source>Prompt Volume</source>
         <translation>耳机提示音量</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="353"/>
         <source>Shutdown timer</source>
+        <translation type="vanished">定时关机</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.ui" line="360"/>
+        <source>Shutdown Timer</source>
         <translation>定时关机</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="391"/>
+        <location filename="devices/basedevice.ui" line="400"/>
         <source>min</source>
         <translation>分钟</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="408"/>
+        <location filename="devices/basedevice.ui" line="416"/>
+        <source>Resets to default state after power cycled</source>
+        <translation>重启后恢复为默认值</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.ui" line="426"/>
         <source>LDAC</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="416"/>
+        <location filename="devices/basedevice.ui" line="434"/>
         <source>OFF</source>
         <translation>关闭</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="423"/>
+        <location filename="devices/basedevice.ui" line="441"/>
         <source>44.1k/48k</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="430"/>
+        <location filename="devices/basedevice.ui" line="448"/>
         <source>96k</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="439"/>
+        <location filename="devices/basedevice.ui" line="457"/>
         <source>Takes effect after re-pairing</source>
         <translation>重新配对后生效</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="452"/>
+        <location filename="devices/basedevice.ui" line="467"/>
+        <source>Changing this will close &quot;Shutdown Timer&quot;</source>
+        <translation>更改此设置将会关闭“定时关机”</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.ui" line="477"/>
         <source>Game Mode</source>
         <translation>游戏模式</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="459"/>
+        <location filename="devices/basedevice.ui" line="484"/>
         <source>Auto power off if there is no audio input for 30 minutes</source>
         <oldsource>Auto power off if there is no audio input for 30 minutes.</oldsource>
         <translation>无音频输入30分钟后自动关机</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="466"/>
+        <location filename="devices/basedevice.ui" line="491"/>
         <source>Poweroff</source>
         <translation>关机</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="473"/>
+        <location filename="devices/basedevice.ui" line="498"/>
         <source>Disconnect</source>
         <translation>断开连接</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="480"/>
+        <location filename="devices/basedevice.ui" line="505"/>
         <source>Re-pair</source>
         <translation>重新配对</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="487"/>
+        <location filename="devices/basedevice.ui" line="512"/>
         <source>Factory Reset</source>
         <translation>恢复出厂设置</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="494"/>
+        <location filename="devices/basedevice.ui" line="519"/>
         <source>Play Control</source>
         <translation>播放控制</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="500"/>
+        <location filename="devices/basedevice.ui" line="525"/>
         <source>Vol+</source>
         <translation>音量+</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="507"/>
+        <location filename="devices/basedevice.ui" line="532"/>
         <source>Vol-</source>
         <translation>音量-</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="514"/>
+        <location filename="devices/basedevice.ui" line="539"/>
         <source>Play</source>
         <translation>播放</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="521"/>
+        <location filename="devices/basedevice.ui" line="546"/>
         <source>Pause</source>
         <translation>暂停</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="528"/>
+        <location filename="devices/basedevice.ui" line="553"/>
         <source>Prev</source>
         <translation>上一曲</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="535"/>
+        <location filename="devices/basedevice.ui" line="560"/>
         <source>Next</source>
         <translation>下一曲</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="545"/>
+        <location filename="devices/basedevice.ui" line="570"/>
         <source>Send Command</source>
         <translation>发送命令</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="551"/>
+        <location filename="devices/basedevice.ui" line="576"/>
         <source>command(Hex)</source>
         <translation>命令(16进制)</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="558"/>
+        <location filename="devices/basedevice.ui" line="583"/>
         <source>Raw</source>
         <translation>原始命令</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.ui" line="565"/>
+        <location filename="devices/basedevice.ui" line="590"/>
         <source>Send</source>
         <translation>发送</translation>
     </message>
     <message>
-        <location filename="devices/basedevice.cpp" line="196"/>
+        <location filename="devices/basedevice.ui" line="600"/>
+        <source>Settings &lt;-&gt; File</source>
+        <translation>设置 &lt;-&gt; 文件</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.ui" line="606"/>
+        <source>Save to File</source>
+        <translation>保存到文件</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.ui" line="613"/>
+        <source>Restore to Device</source>
+        <translation>恢复到设备</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.cpp" line="188"/>
+        <source>The device will be powered off
+Continue?</source>
+        <translation>设备将会关机
+继续吗？</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.cpp" line="194"/>
+        <source>The device will be disconnected
+Continue?</source>
+        <translation>设备将会断开连接
+继续吗？</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.cpp" line="200"/>
+        <source>The device will get into pairing state
+Continue?</source>
+        <translation>设备将会进入配对模式
+继续吗？</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.cpp" line="206"/>
+        <source>The device will be reseted
+Continue?</source>
+        <translation>设备将会恢复出厂设置
+继续吗？</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.cpp" line="220"/>
         <source>The name is too long</source>
         <translation>设备名过长</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.cpp" line="489"/>
+        <source>Save Settings</source>
+        <translation>保存设置</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.cpp" line="533"/>
+        <location filename="devices/basedevice.cpp" line="555"/>
+        <location filename="devices/basedevice.cpp" line="562"/>
+        <location filename="devices/basedevice.cpp" line="568"/>
+        <source>Error</source>
+        <translation>错误</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.cpp" line="533"/>
+        <source>Failed to save to</source>
+        <translation>无法保存至</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.cpp" line="537"/>
+        <source>Saved</source>
+        <translation>已保存</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.cpp" line="555"/>
+        <source>Failed to open</source>
+        <translation>无法打开</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.cpp" line="562"/>
+        <source>Invalid JSON file</source>
+        <translation>无效的JSON文件</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.cpp" line="568"/>
+        <source>Invalid format</source>
+        <translation>文件格式错误</translation>
+    </message>
+    <message>
+        <location filename="devices/basedevice.cpp" line="595"/>
+        <source>Done</source>
+        <translation>完成</translation>
     </message>
 </context>
 <context>
     <name>CommBLE</name>
     <message>
-        <location filename="comms/commble.cpp" line="115"/>
+        <location filename="comms/commble.cpp" line="116"/>
         <source>Device Connected</source>
         <translation>设备已连接</translation>
     </message>
     <message>
-        <location filename="comms/commble.cpp" line="174"/>
+        <location filename="comms/commble.cpp" line="176"/>
         <source>Device Disconnected</source>
         <translation>设备已断开</translation>
     </message>
@@ -260,6 +371,34 @@
         <location filename="comms/commrfcomm.cpp" line="41"/>
         <source>Device Disconnected</source>
         <translation>设备已断开</translation>
+    </message>
+</context>
+<context>
+    <name>DevForm</name>
+    <message>
+        <location filename="devform.ui" line="14"/>
+        <source>Dev Output</source>
+        <translation>调试输出</translation>
+    </message>
+    <message>
+        <location filename="devform.ui" line="35"/>
+        <source>Verbose</source>
+        <translation>显示详细信息</translation>
+    </message>
+    <message>
+        <location filename="devform.ui" line="42"/>
+        <source>Clear</source>
+        <translation>清空</translation>
+    </message>
+    <message>
+        <location filename="devform.ui" line="49"/>
+        <source>Copy</source>
+        <translation>复制</translation>
+    </message>
+    <message>
+        <location filename="devform.cpp" line="25"/>
+        <source>Copied</source>
+        <translation>已复制</translation>
     </message>
 </context>
 <context>
@@ -295,26 +434,24 @@
         <translation>类型</translation>
     </message>
     <message>
-        <location filename="deviceform.ui" line="91"/>
+        <location filename="deviceform.ui" line="95"/>
         <source>Connect</source>
         <translation>连接</translation>
     </message>
     <message>
-        <location filename="deviceform.ui" line="98"/>
+        <location filename="deviceform.ui" line="102"/>
         <source>Disconnect</source>
         <translation>断开连接</translation>
     </message>
     <message>
-        <location filename="deviceform.cpp" line="20"/>
         <location filename="deviceform.cpp" line="109"/>
-        <location filename="deviceform.cpp" line="223"/>
+        <location filename="deviceform.cpp" line="251"/>
         <source>RFCOMM</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="deviceform.cpp" line="21"/>
         <location filename="deviceform.cpp" line="107"/>
-        <location filename="deviceform.cpp" line="221"/>
+        <location filename="deviceform.cpp" line="249"/>
         <source>BLE</source>
         <translation></translation>
     </message>
@@ -322,6 +459,11 @@
         <location filename="deviceform.cpp" line="55"/>
         <source>Bluetooth is not available</source>
         <translation>蓝牙不可用</translation>
+    </message>
+    <message>
+        <location filename="deviceform.cpp" line="169"/>
+        <source>Not a valid Bluetooth address</source>
+        <translation>蓝牙地址无效</translation>
     </message>
 </context>
 <context>
@@ -337,32 +479,42 @@
         <translation>读取设置</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="29"/>
+        <location filename="mainwindow.cpp" line="57"/>
         <source>Device</source>
         <translation>设备</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="119"/>
+        <location filename="mainwindow.cpp" line="148"/>
         <source>Device not connected</source>
         <translation>设备未连接</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="193"/>
+        <location filename="mainwindow.cpp" line="227"/>
         <source>Device detected</source>
         <translation>识别到设备</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="200"/>
+        <location filename="mainwindow.cpp" line="242"/>
+        <source>Dev Mode ON</source>
+        <translation>调试模式已启用</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="249"/>
+        <source>Dev Mode OFF</source>
+        <translation>调试模式已关闭</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="270"/>
         <source>Generic Device</source>
         <translation>通用设备</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="201"/>
+        <location filename="mainwindow.cpp" line="271"/>
         <source>W820NB Double Gold</source>
         <translation>W820NB双金标版</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="202"/>
+        <location filename="mainwindow.cpp" line="272"/>
         <source>W200BT Plus</source>
         <translation></translation>
     </message>
